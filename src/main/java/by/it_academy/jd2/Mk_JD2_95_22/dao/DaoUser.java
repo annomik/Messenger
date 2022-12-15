ced9d4b587f321f16c469b0a2b7700e8d7cd7975
Pavel Klimenko <Horvard777@mail.ru>
2022-12-15 23:54:59 +0200

    Create

---
+++ src/main/java/by/it_academy/jd2/Mk_JD2_95_22/dao/DaoUser.java
@@ -0,0 +1,5 @@
+package by.it_academy.jd2.Mk_JD2_95_22.dao;
+
+public class DaoUser {
+    //хранение в мар, клич, тоже, что и логин
+}
---
+++ src/main/java/by/it_academy/jd2/Mk_JD2_95_22/servise/api/IServiseStatistic.java
@@ -0,0 +1,4 @@
+package by.it_academy.jd2.Mk_JD2_95_22.servise.api;
+
+public interface IServiseStatistic {
+}
