472e59a496d4deac3fc7ba01e2ef6363eaa2dcb1
Dzmitry Bulavitski <dm_bax@mail.ru>
2022-12-16 22:40:48 +0300

    Добавил фабрику MessageServiceSingleton!

---
+++ src/main/java/by/it_academy/jd2/Mk_JD2_95_22/servise/fabrics/MessageServiceSingleton.java
@@ -0,0 +1,19 @@
+package by.it_academy.jd2.Mk_JD2_95_22.servise.fabrics;
+
+import by.it_academy.jd2.Mk_JD2_95_22.servise.MessageService;
+
+public class MessageServiceSingleton {
+
+    private volatile static MessageService instance;
+
+    private MessageServiceSingleton(){}
+    public static MessageService getInstance(){
+        if(instance == null){
+            synchronized (MessageServiceSingleton.class){
+                if(instance == null){
+                    instance = new MessageService();
+                }
+            }
+        } return instance;
+    }
+}
