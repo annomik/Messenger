5221796f7afce5279847b0eaceccb3021f2ab3f5
Hanna Mikulich <mikulichhanna@gmail.com>
2022-12-16 23:58:17 +0300

    Добавила UserDaoSingleton

---
+++ src/main/java/by/it_academy/jd2/Mk_JD2_95_22/dao/fabrics/UserDaoSingleton.java
@@ -0,0 +1,20 @@
+package by.it_academy.jd2.Mk_JD2_95_22.dao.fabrics;
+
+import by.it_academy.jd2.Mk_JD2_95_22.dao.MessageDao;
+import by.it_academy.jd2.Mk_JD2_95_22.dao.UserDao;
+
+public class UserDaoSingleton {
+
+    private volatile static UserDao instance;
+
+    private UserDaoSingleton(){}
+    public static UserDao getInstance(){
+        if(instance == null){
+            synchronized (UserDaoSingleton.class){
+                if(instance == null){
+                    instance = new UserDao();
+                }
+            }
+        } return instance;
+    }
+}
