1f158075d19178d00dee01b538a303a266735495
Hanna Mikulich <mikulichhanna@gmail.com>
2022-12-16 23:18:04 +0300

    Добавила RegistrationServlet

---
+++ src/main/java/by/it_academy/jd2/Mk_JD2_95_22/controllers/RegistrationServlet.java
@@ -0,0 +1,18 @@
+package by.it_academy.jd2.Mk_JD2_95_22.controllers;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "RegistrationServlet", urlPatterns = "/api/user")
+public class RegistrationServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html; charset=UTF-8");
+    }
+}
